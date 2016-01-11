f5758499a4825c8b7f587e8f55019758e64707a8
zxsong13 <zxsong13@gmail.com>
2016-01-11 15:42:18 +0800

    添加轮播广告图片

--- app/src/main/java/com/zxsong/mymall/fragment/HomeFragment.java
+++ app/src/main/java/com/zxsong/mymall/fragment/HomeFragment.java
@@ -3,20 +3,117 @@ package com.zxsong.mymall.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.Indicators.PagerIndicator;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
 import com.zxsong.mymall.R;
 
 /**
  * Created by zxsong on 2015/12/31.
  */
 public class HomeFragment extends Fragment {
+
+    private SliderLayout mSliderLayout;
+
+    private PagerIndicator mIndicator;
+
+    private static final String TAG="HomeFragment";
+
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        mSliderLayout = (SliderLayout) view.findViewById(R.id.slider);
 
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        mIndicator = (PagerIndicator) view.findViewById(R.id.custom_indicator);
+
+        initSlider();
+        return view;
+    }
+
+    private void initSlider() {
+        TextSliderView textSliderView1 = new TextSliderView(this.getActivity());
+        textSliderView1.image("http://m.360buyimg" +
+                ".com/mobilecms/s300x98_jfs/t2416/102/20949846/13425/a3027ebc/55e6d1b9Ne6fd6d8f.jpg");
+        textSliderView1.description("新品推荐");
+
+        textSliderView1.setOnSliderClickListener(new BaseSliderView.OnSliderClickListener() {
+            @Override
+            public void onSliderClick(BaseSliderView baseSliderView) {
+                Toast.makeText(HomeFragment.this.getActivity(), "新品推荐", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        TextSliderView textSliderView2 = new TextSliderView(this.getActivity());
+        textSliderView2.image("http://m.360buyimg" +
+                ".com/mobilecms/s300x98_jfs/t1507/64/486775407/55927/d72d78cb/558d2fbaNb3c2f349.jpg");
+        textSliderView2.description("时尚男装");
+
+        textSliderView2.setOnSliderClickListener(new BaseSliderView.OnSliderClickListener() {
+            @Override
+            public void onSliderClick(BaseSliderView baseSliderView) {
+                Toast.makeText(HomeFragment.this.getActivity(), "时尚男装", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        TextSliderView textSliderView3 = new TextSliderView(this.getActivity());
+        textSliderView3.image("http://m.360buyimg" +
+                ".com/mobilecms/s300x98_jfs/t1363/77/1381395719/60705/ce91ad5c/55dd271aN49efd216.jpg");
+        textSliderView3.description("家电秒杀");
+
+        textSliderView3.setOnSliderClickListener(new BaseSliderView.OnSliderClickListener() {
+            @Override
+            public void onSliderClick(BaseSliderView baseSliderView) {
+                Toast.makeText(HomeFragment.this.getActivity(), "家电秒杀", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mSliderLayout.addSlider(textSliderView1);
+        mSliderLayout.addSlider(textSliderView2);
+        mSliderLayout.addSlider(textSliderView3);
+
+        //转场动画
+        mSliderLayout.setPresetTransformer(SliderLayout.Transformer.RotateUp);
+        // mSliderLayout.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+        //自定义的指示器
+        mSliderLayout.setCustomIndicator(mIndicator);
+        //描述信息的动画
+        mSliderLayout.setCustomAnimation(new DescriptionAnimation());
+        mSliderLayout.setDuration(3000);
+
+        mSliderLayout.addOnPageChangeListener(new ViewPagerEx.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+                Log.d(TAG, "onPageScrolled");
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                Log.d(TAG, "onPageSelected");
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+                Log.d(TAG, "onPageScrollStateChanged");
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        mSliderLayout.stopAutoCycle();
     }
 }
